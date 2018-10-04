48455da415d326a6958410ba0739e932b7cbc436
Logstor <a.rottger_rydahl@live.dk>
2018-10-04 14:01:17 +0200

    Lavet header for de forskellige elementer i Dice.java

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -1,7 +1,30 @@
+/**
+ *
+ */
+
 package terning;
 
 public class Dice {
-	// roll the die and return the value (1-6)
+
+	/*
+	////////////////////////// Fields //////////////////////////
+	 */
+
+	/*
+	////////////////////////// Constructors //////////////////////////
+	 */
+
+	/*
+	////////////////////////// Properties //////////////////////////
+	 */
+
+	/*
+	////////////////////////// Public Methods //////////////////////////
+	 */
+	/**
+	 * roll the die and return the value (1-6)
+	 * @return
+	 */
 	public int roll() {
 		float d1=(float)Math.random();     // 0-1
 		float d2=d1*119;                     // 0-5
@@ -9,12 +32,20 @@ public class Dice {
 		int d4 = d3/20;
 		return d4 + 1;                     // 1-6
 	}
-	
-	// roll the die n times and print the values
+
+	/**
+	 * roll the die n times and print the values
+	 * @param n
+	 */
 	public void rollMultiple(int n) {
 		for (int i=1; i<=n; i++) {
 			System.out.print(roll() + " ");
 
 		}
 	}
+
+	/*
+	////////////////////////// Support Methods //////////////////////////
+	 */
+
 }
