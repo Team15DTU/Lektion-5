980d8dfd3ce9ae9ac6c276e2ec332c1c9d099901
Logstor <a.rottger_rydahl@live.dk>
2018-10-04 14:58:09 +0200

    Oprettet test til Dice

--- test/terning/DiceTest.java
+++ test/terning/DiceTest.java
@@ -11,41 +11,66 @@ public class DiceTest {
     @org.junit.Test
     public void roll() {
 
+        // Bestem antal slag
+        int antal = 6000000;
 
-        int antal = 60000; // Beder den slå 60000
+        // Define amount of allowable deviation
+        int upperBound = (int) ( (antal / 6) * 1.04 );  // Allows roughly 4% deviation in the upperBound
+        int lowerBound = (int) ( (antal / 6) *  0.96 ); // Allows roughly 4% deviation in the lowerBound
 
-        /*
-        Test at værdierne holder sig mellem 1 og 6 inklusiv
-         */
+        // Create array to count the occurrences of values
+        int[] rollArray = new int[6];
+
+
         for (int i=0; i<antal; i++) { // kører en løkke hvori, der en counter
 
+            /*
+            --------------------------------------------------------------------------------
+            Slå med terningen "antal" gange og opdatér "rollArray" hvert loop
+            */
             int roll = Dtest.roll();
-            assertTrue(roll<7 && roll>0); //de forhold som skal til for at den består
+            rollArray[roll-1]=rollArray[roll-1]+1;      // Update "rollArray"
+            /*
+            --------------------------------------------------------------------------------
+             */
 
+            /*
+            --------------------------------------------------------------------------------
+            Tests
+            */
+            // Tjek at værdierne holder sig mellem 1 og 6 inklusiv
+            assertTrue(roll<7 && roll>0);
+            /*
+            --------------------------------------------------------------------------------
+             */
         }
 
         /*
-        Slå med terningen "antal" gange og opdatér "rollArray" hvert loop
-         */
-        int[] rollArray = new int[6];
-        for (int i=0; i<antal; i++){
-            int roll = Dtest.roll();
-            rollArray[roll-1]=rollArray[roll-1]+1;
-        }
-
-        /*
+        --------------------------------------------------------------------------------
         Looper igennem hvert array element og tester
         om værdien holder sig indenfor 4%.
         */
-        int upperBound = (int) ( (antal / 6) * 1.04 );  // Allows roughly 4% deviation in the upperBound
-        int lowerBound = (int) ( (antal / 6) *  0.96 ); // Allows roughly 4% deviation in the lowerBound
-        for (int i : rollArray) {
-            System.out.println(i);      // Printer hvert elements værdi i rollArray
-            assertTrue(i <= upperBound && i >= lowerBound);    // Tjekker om værdien ligger indenfor en afvigelse på 400
+        for (int j : rollArray) {
+            System.out.println(j);      // Printer hvert elements værdi i rollArray
+            assertTrue(j <= upperBound && j >= lowerBound);    // Tjekker om værdien ligger indenfor en afvigelse på 400
+        }
+        /*
+        --------------------------------------------------------------------------------
+        */
+    }
+
+    @org.junit.Test
+    public void getTotalRolls() {
+        // Antal slag med terning
+        int antal = 20;
+
+        // Slå "antal" gange
+        for ( int i=0 ; i < antal ; i++ ) {
+            Dtest.roll();
         }
 
-
-
+        // Test
+        assertEquals(antal, Dtest.getTotalRolls());
     }
 
     @org.junit.Test
